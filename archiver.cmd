3b0c987cae10c6c1bad21d1da52dd3f0473063f9
Jens Jap <jens_jap@brown.edu>
2013-05-07 14:35:28 -0400

    Script looks for files older than 90 days and zips only those.

--- archiver.cmd
+++ archiver.cmd
@@ -1,4 +1,3 @@
-
 @echo off
 setlocal
 setlocal enabledelayedexpansion
@@ -7,16 +6,15 @@ rem *^*^*^*^*^*^*^*^*^*^*^*
 rem Jens Jap - 2013.05.06
 rem Beer license applies
 rem *^*^*^*^*^*^*^*^*^*^*^*
-echo.
-echo.
 
 set yyyy_mm_dd=%date:~-4,4%_%date:~-10,2%_%date:~-7,2%
 set ZIPEX=D:\utilities\zip.exe
-set LOG=D:\utilities\ziperror.txt
+set LOG=D:\utilities\ziperror.log
 set ARCHIVE_DIR=D:\Ruby\srdr-dev\log\archives\
-set CMD="dir /A-D /S /B D:\Ruby\srdr-dev\log\archives\*.log"
+::set CMD="dir /A-D /S /B D:\Ruby\srdr-dev\log\archives\*.log"
+set CMD="forfiles /p d:\Ruby\srdr-dev\log\archives /M *.log /S /C "cmd /c echo @path" /D -90"
 
-echo archiver started - %yyyy_mm_dd% >>%LOG%
+echo ========== archiver started - %yyyy_mm_dd%
 
 if not exist %ZIPEX% goto :ERROR1
 
@@ -30,31 +28,30 @@ if ERRORLEVEL 0 goto :CLEANUP
 goto :ERROR2
 
 :CLEANUP
-for /f %%L in ('%CMD%') do (
+for /f %%L in (' %CMD% ') do (
     set filename=%%L
     echo INFO: Deleting %%L now...
     del %%L
     if exist %%L call :ERROR3
-    echo.
 )
 goto :END
 
 :ERROR1
-echo CRITICAL: zip utility not found in D:\utilities\        >>%LOG%
-echo CRITICAL: Please ensure it exists before trying again   >>%LOG%
-echo CRITICAL: Terminating now                               >>%LOG%
+echo CRITICAL: zip utility not found in D:\utilities\
+echo CRITICAL: Please ensure it exists before trying again
+echo CRITICAL: Terminating now
 goto :END
 
 :ERROR2
-echo CRITICAL: Creating zip file failed                      >>%LOG%
-echo CRITICAL: Terminating now                               >>%LOG%
+echo CRITICAL: Creating zip file failed
+echo CRITICAL: Terminating now
 goto :END
 
 :ERROR3
-echo ERROR: Problem deleting !filename!                      >>%LOG%
+echo ERROR: Problem deleting !filename!
 goto :EOF
 
 :END
-echo archiver finished - %yyyy_mm_dd% >>%LOG%
-echo. >>%LOG%
+echo ========== archiver finished - %yyyy_mm_dd%
+echo.
 endlocal
